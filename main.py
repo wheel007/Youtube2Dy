be0540b7288b75fcac0bf6a9a6de0ef8ee5c7171
Anohana <184395761@qq.com>
2023-02-26 11:03:18 +0800

    2023.2.26 - 完成YouTube视频下载功能 - 完成字幕文件下载功能

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import tkinter as tk
 from pytube import YouTube
 from tkinter import ttk
-from googletrans import Translator
-import pysrt
+from pygtrans import Translate
+from youtube_transcript_api import YouTubeTranscriptApi
 
 
 class YouTubeDownloader:
     def __init__(self, master):
         self.master = master
         master.title("YouTube Downloader")
-
+        # 需要科学上网，设置自己的代理
         self.proxy = {
             "http": "http://127.0.0.1:10809",
             "https": "https://127.0.0.1:10809"
         }
 
-        self.requests_get_proxy = {"http": "http://127.0.0.1:10809", "https": "http://127.0.0.1:10809"}
+        self.download_subtitles_proxy = {
+            "http": "http://127.0.0.1:10809",
+            "https": "http://127.0.0.1:10809"
+        }
 
         # 创建Label和Entry
         self.label = tk.Label(master, text="请输入YouTube视频链接:")
@@ -28,6 +31,7 @@ class YouTubeDownloader:
         self.button.grid(row=1, column=1, padx=5, pady=5)
 
         # 创建进度条
+        # TODO 目前进度条不起作用，待调整
         self.progress = ttk.Progressbar(master, orient="horizontal", length=200, mode="determinate")
         self.progress.grid(row=2, column=1, padx=5, pady=5)
 
@@ -53,83 +57,108 @@ class YouTubeDownloader:
 
         yt.register_on_progress_callback(progress_callback)
         filename = stream.download()  # 下载视频到本地
+        # TODO 用控件显示
         print(f"视频下载完成，保存为{filename}")
 
-    def translate_subtitles(self, input_file, output_file):
+    def time_calculate(self, start, duration):
         """
-        翻译字幕
-        :return:
+        读取字幕内容转化为srt文件的时间表示
+        :param start: 开始时间
+        :param duration: 持续时间
+        :return: 返回格式化的字符串，如 00:00:00,000 --> 00:00:01,234
         """
+        # 计算
+        (start_hour, remain) = divmod(start, 3600)
+        (start_min, start_sec) = divmod(remain, 60)
+        end = start + duration
+        (end_hour, remain) = divmod(end, 3600)
+        (end_min, end_sec) = divmod(remain, 60)
 
-        # 输入要翻译成的目标语言
-        target_lang = 'zh-cn'
+        start_hour = int(start_hour)
+        start_min = int(start_min)
+        start_sec = str(int(start_sec * 1000))
+        end_hour = int(end_hour)
+        end_min = int(end_min)
+        end_sec = str(int(end_sec * 1000))
 
-        # 创建Translator对象
-        translator = Translator(service_urls=['translate.google.cn'])
+        if len(str(start_hour)) < 2:
+            start_hour = "0" + str(start_hour)
+        if len(str(start_min)) < 2:
+            start_min = "0" + str(start_min)
+        while len(start_sec) < 5:
+            start_sec = "0" + start_sec
+        str_list = list(start_sec)
+        str_list.insert(2, ",")
+        start_sec = ''.join(str_list)
 
-        # 打开输入文件
-        subs = pysrt.open(input_file)
-        print('subs:'+subs)
+        if len(str(end_hour)) < 2:
+            end_hour = "0" + str(end_hour)
+        if len(str(end_min)) < 2:
+            end_min = "0" + str(end_min)
+        while len(end_sec) < 5:
+            end_sec = "0" + end_sec
+        str_list = list(end_sec)
+        str_list.insert(2, ",")
+        end_sec = ''.join(str_list)
 
-        # 循环遍历每一个字幕并翻译
-        for sub in subs:
-            # 只翻译非中文字幕
-            if not sub.text.isalpha() and not sub.text.isdigit() and not sub.text.isascii():
-                # 翻译字幕
-                print(sub.text)
-                translation = translator.translate(sub.text, dest=target_lang)
-                # 将翻译后的文本保存到字幕对象中
-                sub.text = translation.text
-                print(sub.text)
+        time_string = "{}:{}:{} --> {}:{}:{}".format(start_hour, start_min, start_sec, end_hour, end_min, end_sec)
+        return time_string
 
-        # 将翻译后的字幕保存到输出文件
-        subs.save(output_file, encoding='utf-8')
-
-        print("字幕翻译完成！")
+    def save_srt(self, srt, fileName):
+        count = 1
+        for i in srt:
+            with open("{}.srt".format(fileName), "a", encoding="utf-8") as f:
+                f.write("{}\n".format(count))
+                count += 1
+                second_line = self.time_calculate(i["start"], i["duration"])
+                f.write("{}\n".format(second_line))
+                f.write("{}\n".format(i["text"]))
+                f.write("\n")
 
     def download_subtitles(self):
         """
         下载youtube视频字幕
         :return:
         """
-        url = self.entry.get()
-        # 创建YouTube对象
+        url = self.entry.get()  # 获取用户输入的YouTube链接
         yt = YouTube(url, proxies=self.proxy)
-
-        # 获取视频的所有字幕
-        subtitles = yt.captions
-
-        # 查找中文字幕并下载
-        caption = None
-        for subtitle in subtitles:
-            if subtitle.code == 'zh-Hans':
-                caption = subtitles.get_by_language_code(subtitle.code)
-                break
-
-        # 如果没有中文字幕则选择英文字幕
-        if not caption:
-            for subtitle in subtitles:
-                if subtitle.code == 'en':
-                    caption = subtitles.get_by_language_code(subtitle.code)
-                    break
-
-        # 如果中英文字幕都没有则选第一个可用字幕
-        if not caption:
-            for subtitle in subtitles:
-                caption = subtitles.get_by_language_code(subtitle.code)
-                break
-
-        # 下载字幕
-        if caption:
-
-            subtitle_text = caption.generate_srt_captions()
-            fileName = f"{yt.title}.srt"
-            with open(fileName, "w", encoding="utf-8") as f:
-                f.write(subtitle_text)
-            print("字幕已下载完成！")
-            # self.translate_subtitles(fileName, fileName)
+        video_id = url.split('=')[-1]
+        # 获取中文字幕
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id, proxies=self.download_subtitles_proxy)
+        lauguague_code_list = []
+        for transcript in transcript_list:
+            lauguague_code_list.append(transcript.language_code)
+        if "zh-Hans" in lauguague_code_list:
+            srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=self.download_subtitles_proxy, languages=["zh-Hans"])
+            self.save_srt(srt, fileName=yt.title)
+        elif "zh-CN" in lauguague_code_list:
+            srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=self.download_subtitles_proxy, languages=["zh-CN"])
+            self.save_srt(srt, fileName=yt.title)
+        elif "zh-HK" in lauguague_code_list:
+            srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=self.download_subtitles_proxy, languages=["zh-HK"])
+            self.save_srt(srt, fileName=yt.title)
+        elif "zh-TW" in lauguague_code_list:
+            srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=self.download_subtitles_proxy, languages=["zh-TW"])
+            self.save_srt(srt, fileName=yt.title)
+        # elif "en" in lauguague_code_list:
+        #     srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy, languages=["en"])
         else:
-            print("字幕不存在！")
+            # TODO 控件显示
+            print("没有中文字幕，尝试机翻")
+            srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=self.download_subtitles_proxy)
+            client = Translate(proxies=self.download_subtitles_proxy)
+            count = 1
+            for i in srt:
+                with open("{}.srt".format(yt.title), "a", encoding="utf-8") as f:
+                    f.write("{}\n".format(count))
+                    count += 1
+                    second_line = self.time_calculate(i["start"], i["duration"])
+                    f.write("{}\n".format(second_line))
+                    translated = client.translate(i["text"], target="zh-CN").translatedText # 翻译
+                    # TODO 控件显示
+                    print("{} -- > {}".format(i["text"], translated))
+                    f.write("{}\n".format(translated))
+                    f.write("\n")
 
     def run(self):
         self.master.mainloop()
--- test.py
+++ test.py
@@ -1,7 +1,105 @@
-import os
+from pygtrans import Translate
+from youtube_transcript_api import YouTubeTranscriptApi
+proxy = {
+            "http": "http://127.0.0.1:10809",
+            "https": "http://127.0.0.1:10809"
+        }
 
-import pysrt
-subs = pysrt.open("1.srt")
-print(subs)
-f = open("1.srt")
-print(f.read())
+# 输入视频链接
+url = 'https://www.youtube.com/watch?v=8bzsRpWb0t4'
+
+# 从视频链接中提取视频ID
+video_id = url.split('=')[-1]
+
+
+def time_calculate(start, duration):
+    # 计算
+    (start_hour, remain) = divmod(start, 3600)
+    (start_min, start_sec) = divmod(remain, 60)
+    end = start + duration
+    (end_hour, remain) = divmod(end, 3600)
+    (end_min, end_sec) = divmod(remain, 60)
+
+    start_hour = int(start_hour)
+    start_min = int(start_min)
+    start_sec = str(int(start_sec * 1000))
+    end_hour = int(end_hour)
+    end_min = int(end_min)
+    end_sec = str(int(end_sec * 1000))
+
+    if len(str(start_hour))<2:
+        start_hour = "0" + str(start_hour)
+    if len(str(start_min)) < 2:
+        start_min = "0" + str(start_min)
+    while len(start_sec) < 5:
+        start_sec = "0" + start_sec
+    str_list = list(start_sec)
+    str_list.insert(2, ",")
+    start_sec = ''.join(str_list)
+
+    if len(str(end_hour)) < 2:
+        end_hour = "0" + str(end_hour)
+    if len(str(end_min)) < 2:
+       end_min = "0" + str(end_min)
+    while len(end_sec) < 5:
+        end_sec = "0" + end_sec
+    str_list = list(end_sec)
+    str_list.insert(2, ",")
+    end_sec = ''.join(str_list)
+
+    time_string = "{}:{}:{} --> {}:{}:{}".format(start_hour, start_min, start_sec, end_hour, end_min, end_sec)
+    return time_string
+
+
+def save_srt(srt, fileName):
+    count = 1
+    for i in srt:
+        with open("{}.srt".format(fileName), "a", encoding="utf-8") as f:
+            f.write("{}\n".format(count))
+            count += 1
+            second_line = time_calculate(i["start"], i["duration"])
+            f.write("{}\n".format(second_line))
+            f.write("{}\n".format(i["text"]))
+            f.write("\n")
+
+# 获取视频的字幕信息
+transcript_list = YouTubeTranscriptApi.list_transcripts(video_id, proxies=proxy)
+lauguague_code_list = []
+for transcript in transcript_list:
+    lauguague_code_list.append(transcript.language_code)
+if "zh-Hans" in lauguague_code_list:
+    srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy, languages=["zh-Hans"])
+    save_srt(srt, fileName="test")
+elif "zh-CN" in lauguague_code_list:
+    srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy, languages=["zh-CN"])
+    save_srt(srt, fileName="test")
+elif "zh-HK" in lauguague_code_list:
+    srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy, languages=["zh-HK"])
+    save_srt(srt, fileName="test")
+elif "zh-TW" in lauguague_code_list:
+    srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy, languages=["zh-TW"])
+    save_srt(srt, fileName="test")
+# elif "en" in lauguague_code_list:
+#     srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy, languages=["en"])
+else:
+    print("没有中文字幕，尝试机翻")
+    srt = YouTubeTranscriptApi.get_transcript(video_id, proxies=proxy)
+    # for i in srt_list:
+    #     if i.is_translatable():
+    #         srt = i.translate('zh-Hans').fentch()
+    #         break
+    client = Translate(proxies=proxy)
+    count = 1
+    for i in srt:
+        with open("{}.srt".format("test"), "a", encoding="utf-8") as f:
+            f.write("{}\n".format(count))
+            count += 1
+            second_line = time_calculate(i["start"], i["duration"])
+            f.write("{}\n".format(second_line))
+            translated = client.translate(i["text"], target="zh-CN").translatedText
+            print("{} -- > {}".format(i["text"], translated))
+            # i["text"] = translated
+            f.write("{}\n".format(translated))
+            f.write("\n")
+
+
